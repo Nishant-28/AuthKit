401aca28620f7f3b26de67302401397af2b8fc5b
Nishant <inet.nishant@gmail.com>
2026-01-26 23:59:35 +0530

    feat: setup user domain and repository

---
+++ src/main/java/com/nishant/AuthKit/entity/Role.java
@@ -0,0 +1,6 @@
+package com.nishant.AuthKit.entity;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/nishant/AuthKit/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nishant.AuthKit.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nishant.AuthKit.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
